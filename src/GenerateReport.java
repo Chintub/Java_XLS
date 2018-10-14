d0ff64fd24b9e123afc1f4a1b077dbf35919e877
Chintub <manikanth.edu@gmail.com>
2018-10-14 12:25:04 +0530

    Entire project is added

---
+++ src/GenerateReport.java
@@ -0,0 +1,10 @@
+import org.automationtesting.excelreport.Xl;
+
+public class GenerateReport {
+
+	public static void main(String[] args) throws Exception
+    {
+     Xl.generateReport("excel-report.xlsx");
+    }
+
+}
